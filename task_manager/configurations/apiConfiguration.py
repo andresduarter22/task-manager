2245f7867bc8fe25bcd48e61ecf33a5d8d45b956
René Vilar Schlichter <renevs91@gmail.com>
2021-05-06 15:56:30 -0400

    TASK-214: Updated API configuration

--- task_manager/configurations/apiConfiguration.py
+++ task_manager/configurations/apiConfiguration.py
@@ -9,6 +9,8 @@ class ApiConfiguration(QueryBasedConfiguration):
 
     Attributes
     ----------
+    load_default : bool
+        defines if the default config is loaded
     credentials : dict
         the login credentials
     url : string
@@ -21,11 +23,18 @@ class ApiConfiguration(QueryBasedConfiguration):
     set_up():
         Prepares the configuration for being used when executing the task.
     """
-    def __init__(self, credentials: dict, url: str, r_type: str):
-        self.credentials = credentials
-        self.url = url
-        self.r_type = r_type
-
+    def __init__(self, load_default: bool, credentials: dict = None, url: str = None, r_type: str = None):
+        if load_default:
+            import json
+            with open(defs.PATHS['default_api_task_config']) as json_file:
+                data = json.load(json_file)
+                self.credentials = data['credentials']
+                self.url = data['url']
+                self.r_type = data['r_type']
+        else:
+            self.credentials = credentials
+            self.url = url
+            self.r_type = r_type
 
     def set_up(self):
         """
