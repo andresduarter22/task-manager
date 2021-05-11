97a75067c9aa2f824e9df65d418e54648353f834
Rene Vilar <renevs91gmail.com>
2021-05-11 19:23:07 -0400

    DevOps: shell script v1

--- task_manager/utils/exceptions.py
+++
@@ -1,9 +0,0 @@
-from flask_restful import abort
-
-
-class CustomException(Exception):
-
-    @staticmethod
-    def abort_if_entry_doesnt_exist(id_entry, collection):
-        if collection.find_one({"_id": int(id_entry)}) is None:
-            abort(400, message="Entry doesn't exist")
