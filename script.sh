75b4be857baeb27ecbfa886999e6c2c459555f78
andresduarter22 <LeCuack22>
2021-05-13 13:07:20 -0400

    file name is script.sh repaired, adding more requirements to requirements.txt

--- script.sh
+++ script.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
+sudo apt install python3-pip
+sudo apt install python3-venv
+wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+sudo apt -y install mongodb-org
+
+DIR="./venv"
+if [ ! -d "$DIR" ]; then
+  python3 -m venv venv
+fi
 source ./venv/bin/activate
 export PYTHONPATH="$(pwd)"
-./venv/bin/python tests/test_db_task.py
-./venv/bin/python -m build
-./venv/bin/python task_manager/app.py
+pip3 install -r requirements.txt
+./venv/bin/python3 -m build
+./venv/bin/python3 tests/test_db_task.py
+./venv/bin/python3 task_manager/app.py
