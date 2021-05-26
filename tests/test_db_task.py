9aed384ad96b5221d14434b50e03c301153c4b05
andresduarter22 <LeCuack22>
2021-05-26 19:18:16 -0400

    Fixing test_db_task.py

--- tests/test_db_task.py
+++ tests/test_db_task.py
@@ -6,13 +6,13 @@ class Evaluate(unittest.TestCase):
 
     def test_insert(self):
         data = {"_id": 2, "First name": "Andres"}
-        mongo = MongoDbConnector("localhost,27017,db_task,test", [data])
+        mongo = MongoDbConnector("mongo,27017,db_task,test", [data])
         actual = mongo.insert()
         expected = {"response": "Data successfully inserted"}
         self.assertEqual(expected, actual)
 
     def test_select_all(self):
-        mongo = MongoDbConnector("localhost,27017,db_task,test", [])
+        mongo = MongoDbConnector("mongo,27017,db_task,test", [])
         actual = mongo.select_all()
         expected = None
         print("Hello xD")
@@ -20,7 +20,7 @@ class Evaluate(unittest.TestCase):
 
     def test_select(self):
         data = {"_id": 2, "First name": "Andres"}
-        mongo = MongoDbConnector("localhost,27017,db_task,test", ["2"])
+        mongo = MongoDbConnector("mongo,27017,db_task,test", ["2"])
         actual = mongo.select_by_id()
         expected = data
         self.assertEqual(expected, actual)
@@ -34,7 +34,7 @@ class Evaluate(unittest.TestCase):
             {"db": "db_task", "collection": "test"}
             ]
             }
-        mongo = MongoDbConnector("localhost,27017,db_task,test", ["2", data])
+        mongo = MongoDbConnector("mongo,27017,db_task,test", ["2", data])
         actual = mongo.update()
         expected = {
              "response": "Document successfully updated"
@@ -43,7 +43,7 @@ class Evaluate(unittest.TestCase):
 
     def test_delete(self):
         data = {"_id": 2, "First name": "Andres"}
-        mongo = MongoDbConnector("localhost,27017,db_task,test", ["2", data])
+        mongo = MongoDbConnector("mongo,27017,db_task,test", ["2", data])
         actual = mongo.delete()
         expected = {"response": "Document successfully deleted"}
         self.assertEqual(expected, actual)
